46fed8ed83c85d04158f482fe5a6f63fb0120766
Neeraja824 <lankaneeraja824@gmail.com>
2026-01-11 13:25:27 +0530

    files

--- Routes/firstRoute.js
+++ Routes/firstRoute.js
@@ -1,6 +1,17 @@
-const express = require('express');
-const FirstController = require('../Controller/firstController');
-const router = express.Router();
-// router.get('/get-data', FirstController.TestingAPI);
-router.get('/get-data', FirstController.GetData);
-module.exports = router;
+// const express = require('express');
+// const FirstController = require('../Controller/firstController');
+// const router = express.Router();
+// // router.get('/get-data', FirstController.TestingAPI);
+// router.get('/get-data', FirstController.GetData);
+// module.exports = router;
+
+const express = require('express')
+const FirstController = require("../Controller/firstController")
+const route = express.Router();
+
+route.post("/add-data",FirstController.AddData)
+route.get('/get-data',FirstController.GetData)
+route.patch("/update-data/:id",FirstController.UpdateData)
+route.delete("/delete-data/:id",FirstController.DeleteData)
+            
+module.exports = route;
--- server.js
+++ server.js
@@ -7,48 +7,24 @@
 // });
 // app.listen(9000,()=>console.log('Server running on port 9000'));
 
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const FirstRoute = require("./Routes/firstRoute")
+app.use(cors())
 app.use(express.json());
-let students = [];
-let id = 1;
-app.post("/students", (req, res) => {
-  const student = {
-    id: id++,
-    name: req.body.name,
-    roll: req.body.roll,
-    phone: req.body.phone,
-    address: req.body.address
-  };
 
-  students.push(student);
-  res.json(student);
-});
-app.get("/students", (req, res) => {
-  res.json(students);
-});
-app.get("/students/:id", (req, res) => {
-  const student = students.find(s => s.id == req.params.id);
-  res.json(student);
-});
-app.put("/students/:id", (req, res) => {
-  const index = students.findIndex(s => s.id == req.params.id);
+mongoose.connect("mongodb://localhost:27017/")
+.then(res =>{
+    console.log("DB is Connected")
+})
+.catch(err =>{
+    console.log("error in connecting DB",err)
+})
 
-  students[index] = {
-    id: students[index].id,
-    ...req.body
-  };
+app.use("/",FirstRoute);
 
-  res.json(students[index]);
-});
-app.delete("/students/:id", (req, res) => {
-  students = students.filter(s => s.id != req.params.id);
-  res.json({ message: "Student deleted" });
-});
-
-app.listen(3000, () => {
-  console.log("Backend running on http://localhost:3000");
-});
+app.listen(9000,()=>{
+    console.log("Server Started at 9000")
+})
